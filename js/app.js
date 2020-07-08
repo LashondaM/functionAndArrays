69e8a8054e165db699d74c6b0acd5d7fba5edfcc
Lashonda Miller <lashondamiller007@gmail.com>
2020-07-08 01:07:32 -0400

    functions and arrays

---
+++ js/app.js
@@ -0,0 +1,53 @@
+// Functions
+
+var p = 4;
+var b = 5;
+
+//
+
+function add(arg1, arg2) {
+    return arg1 + arg2 ;
+}
+
+console.log(add(p,b));
+
+//
+
+function sub(arg3, arg4) {
+    return arg3 - arg4 ;
+}
+
+console.log(sub(p,b));
+
+//
+
+function multi(arg5, arg6) {
+    return arg5 * arg6 ;
+} 
+
+console.log(multi(p,b));
+
+//
+
+function divi(arg7, arg8) {
+    return arg7 / arg8 ;
+}
+
+console.log(divi(p,b));
+
+
+// Arrays
+
+var x = [["Dye", 1], ["Hair", 2]];
+
+var y = [["Juice", 5] ["Fan", 6]];
+
+var z = [["Boy", 3], ["Girl", 4]];
+
+//
+
+function x(arg9, arg10) {
+x.push(["Toy", 8]);
+}
+
+console.log(x);
